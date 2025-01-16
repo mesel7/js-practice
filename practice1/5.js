902275363a771c143212024e63dbf02299495812
mesel7 <cejhans1520@gmail.com>
2025-01-16 23:54:02 +0900

    C4 practice1 5 for문 사용

--- practice1/5.js
+++ practice1/5.js
@@ -1,8 +1,22 @@
 const myFunc = (arr) => {
     let min = arr[0];
     // 단락 평가
+    // 시간 복잡도 O(n)
     arr.forEach(it => it < min && (min = it));
     return min;
 }
 
-console.log(myFunc([1, 3, 7, 11, 2, 15, 0]));
+const myFunc2 = (arr) => {
+    let min = arr[0];
+    // for문 사용
+    // 시간 복잡도 O(n)
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+console.log(myFunc2([1, 3, 7, 11, 2, 15, 0]));
