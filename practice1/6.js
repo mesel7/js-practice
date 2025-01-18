2a683757d279fa8e4da990e2ddb2fa771b02d2ea
mesel7 <cejhans1520@gmail.com>
2025-01-18 23:49:38 +0900

    6.js myFunc2

--- practice1/6.js
+++ practice1/6.js
@@ -15,4 +15,21 @@ const myFunc = (arr) => {
     console.log(min);
 };
 
-myFunc([12, 77, 38, 41, 53, 92, 85]);
+const myFunc2 = (arr) => {
+    let sum = 0;
+    let min = null;
+
+    arr.forEach((it) => {
+        if (it % 2 != 0) {
+            sum += it; 
+            if (min === null || it < min) {
+                min = it;
+            }
+        }
+    });
+
+    console.log(sum);
+    console.log(min);
+};
+
+myFunc2([12, 77, 38, 41, 53, 92, 85]);
